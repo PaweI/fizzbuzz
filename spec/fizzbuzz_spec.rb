1e83cf0e72a24c209512cb4b8e68c95f3a82e4d1
Pavel Redics <pavel.redics@gmail.com>
2014-10-27 14:35:00 +0000

    First commit

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,25 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do 
+	context 'knows when a number' do
+		it 'is divisible by three' do
+			expect(is_divisible_by_three?(3)).to be true
+		end
+
+		it 'is divisible by five' do 
+			expect(is_divisible_by_five?(5)).to be true
+		end
+
+		it 'is divisible by fifteen' do
+			expect(is_divisible_by_fifteen?(15)). to be true
+		end
+
+		it 'is NOT divisible by three' do
+			expect(is_divisible_by_three?(3)).to be true
+		end
+
+		it  'is NOT divisible by five' do
+			expect(is_divisible_by_five?(1)).to be false
+		end
+	end
+end
