6f8f4bcc79f53b7218ab7e5dcc023d48464f67b0
Pavel Redics <pavel.redics@gmail.com>
2014-10-27 15:43:27 +0000

    fizzbuzz spec, full

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -23,3 +23,21 @@ describe 'fizzbuzz' do
 		end
 	end
 end
+
+describe 'when playing the game says' do
+	it '"Fizz" when number is divisible by three' do
+		expect(fizzbuzz_says(3)).to eq "Fizz"
+	end
+
+	it '"Buzz" when a number is divisible by five' do
+		expect(fizzbuzz_says(5)).to eq "Buzz"
+	end
+
+	it '"FizzBuzz" when number is is_divisible_by_fifteen' do
+		expect(fizzbuzz_says(15)).to eq "FizzBuzz"
+	end
+
+	it 'returns number if NOT divisible by 3,5 or 15' do
+		expect(fizzbuzz_says(1)).to eq 1
+	end
+end
