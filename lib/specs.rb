b5ebd496a129f129d87a298541a6596d4d2bb51f
Pavel Redics <pavel.redics@gmail.com>
2014-10-27 18:50:56 +0000

    Max and min value

--- lib/specs.rb
+++ lib/specs.rb
@@ -5,3 +5,11 @@ end
 def sentence_reverse(sentence)
   sentence.downcase.split.reverse.join(' ').capitalize
 end
+
+def min_value(array)
+  array.min 
+end
+
+def max_value(array)
+  array.max 
+end
--- spec/specs_spec.rb
+++ spec/specs_spec.rb
@@ -8,7 +8,14 @@ describe 'specs' do
   it 'reverse a sentence' do
     expect(sentence_reverse("Pizza is dough")).to eq "Dough is pizza"
   end
-  
+
+  it 'finds minimum value in a list' do
+    expect(min_value([12, 3, 5, 7, 9, 234, 90])).to eq 3 
+  end
+
+  it 'finds maximum value in a list' do
+    expect(max_value([12, 3, 5, 7, 9, 234, 90])).to eq 234
+  end
 
 
 end
